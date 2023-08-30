3b6f52eb8a8061806e41276d4509b7f959a3edcb
Mikayel <mikayel1999@mail.com>
2023-08-30 14:56:07 +0000

    Update App.js
    Working With Event Handlers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React from'react';
+import React from "react";
 
 class Counter extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      count: 5
-    }; 
+      count: 5,
+    };
+
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
   }
 
-  render() { // the "this" here will point to the current component instance
+  handleDecrement() {
+    // this.setState({
+    //   count: this.state.count - 1
+    // });
+    this.setState(curState => {
+      return { count: curState.count - 1 }
+    });
+  }
+  
+  handleIncrement() {
+    this.setState(curState => {
+      return { count: curState.count + 1 }
+    });
+  }
+
+  render() {
+    // the "this" here will point to the current component instance
+    const date = new Date('june 21 2027')
+    date.setDate(date.getDate() + this.state.count)
+    
     return (
       <div className="counter">
-        <button>Counter: {this.props.count}</button>
-        <span>{this.state.count}</span> 
-        <button>Counter: {this.props.count}</button>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>{date.toDateString()} [{this.state.count}]</span>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
